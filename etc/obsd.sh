9185774d45b71d29726ae44c276a64f261a2b1f0
Gunpachi <37960846+Gunpachi@users.noreply.github.com>
2020-10-31 00:32:47 +0530

    Added new st rpo clone line
    Also organized the stuff a bit.

--- etc/obsd.sh
+++ etc/obsd.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/sh
-pkg_add openbox tint2 obconf tint2conf firefox nnn st neovim pulseaudio fish mpv mpd ncmpcpp slim
+
+#Install Necessary apps
+pkg_add openbox tint2 obconf tint2conf firefox nnn neovim pulseaudio fish mpv mpd ncmpcpp slim
+
+#Enables slim dm and disables the default Xenodm
 rcctl enable slim
 rcctl disable xenodm
 echo "Edit /etc/slim.conf to your needs"
+
+#Create Directories & clone my st Repo and make install it
+mkdir -p Documents Downloads GitRepos/{MyRepos,Repos} Music Videos 
+cd /home/gunpachi/GitRepos/MyRepos && doas -u gunpachi git clone https://github.com/Gunpachi/st.git
+cd st && make && make clean install
+
